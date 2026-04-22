0b31c582b15f81a6703680c4039649d1c24bcfe8
sidnou <sidjack972@gmail.com>
2026-04-22 13:36:22 +0200

    Ajout des modèles Experience et Competences avec leurs champs associés dans models.py, et enregistrement des modèles dans admin.py

--- portfolio/apps/core/admin.py
+++ portfolio/apps/core/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Experience, Competences
+
 
 # Register your models here.
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Competences)
+class CompetencesAdmin(admin.ModelAdmin):
+    ...
--- portfolio/apps/core/models.py
+++ portfolio/apps/core/models.py
@@ -3,13 +3,24 @@ from django.db import models
 
 # Create your models here.
 class Experience(models.Model):
+
+    TYPE_POSTE = [
+        ('CDI','CDI'),
+        ('CDD','CDD'),
+        ('MISSION','MISSION'),
+    ]
+    entreprise = models.CharField(max_length=50)
     date_debut = models.DateField()
-    
-    ...
+    date_fin = models.DateField(null=True,blank=True)
+    poste= models.CharField(max_length=25)
+    fonctions_roles = models.CharField(max_length=250)
+    type_poste = models.CharField(max_length=25,choices=TYPE_POSTE)
+
 
 
 class Competences(models.Model):
-    ...
+    experiences = models.ManyToManyField(Experience)
+
 
 
 class Certificat(models.Model):
